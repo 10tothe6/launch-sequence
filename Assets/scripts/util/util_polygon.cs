2b43c11cf55e1fd076af643ea5ab809ce6d3b58e
Maximilian McDiarmid <maximilianmcdiarmid@icloud.com>
2026-03-09 11:38:53 -0400

    implemented non-convex triangulation algorithm, untested though

--- Assets/scripts/util/util_polygon.cs
+++ Assets/scripts/util/util_polygon.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 // struct representing a piece of a polygon,
 // so that we can keep track of vertex indices
+
+// (UNUSED RIGHT NOW)
 [System.Serializable]
 public class util_polygoncomponent
 {
-    
+    public Vector2[] vertices;
+    public Vector2[] actualVertexIndices;
+
+    public util_polygoncomponent() {}
+
+    public util_polygoncomponent(Vector2[] vertices, Vector2[] actualVertexIndices)
+    {
+        this.vertices = vertices;
+        this.actualVertexIndices = actualVertexIndices;
+    }
 }
 
 public class util_polygon
@@ -17,9 +28,12 @@ public class util_polygon
     // * cut up the concave shape into a bunch of convex shapes
     // * triangulate all the convex shapes
     // * combine those triangulations into one final thing
+
+    // it was painful, but this is non-recursive (as in it doesn't call itself)
     public static int[] GenerateConcaveTriangulation(Vector2[] verts)
     {
         // gotta convert to 3D for our little angle-detection trick
+        // pretty much everything up until the very end will reference this
         Vector3[] verts3D = new Vector3[verts.Length];
         for (int i = 0; i < verts.Length; i++)
         {
@@ -27,43 +41,108 @@ public class util_polygon
         }
 
         // to be filled with all of the convex pieces of our concave shape
-        List<Vector2[]> convexSlices = new List<Vector2[]>();
+        // think of these as 'fully processed'
+        List<int[]> convexSlices = new List<int[]>(); // ints are vertex indices
+
+        List<int[]> concaveSlices = new List<int[]>();
+        // we add the shape as a whole as a concave slice, and begin
 
         Vector3 normal = Vector3.up;
 
-        for (int i = 0; i < verts.Length; i++)
+        while (concaveSlices.Count > 0)
         {
-            Vector3 v1 = GetPreceedingVertex(verts3D, i);
-            Vector3 v2 = verts[i];
-            Vector3 v3 = GetSucceedingVertex(verts3D, i);
-
-            Vector3 a = v1 - v2;
-            Vector3 b = v3 - v2;
-
-            float cAngle = GetClockwiseAngle(a, b, normal);
-
-            if (cAngle >= 180)
+            // we loop backwards cuz things will be getting deleted and we don't wanna mess up the index math
+            for (int n = concaveSlices.Count - 1; n >= 0; n--)
             {
-                Vector2 a2D = new Vector2(a.x, a.z);
-                Vector2 b2D = new Vector2(b.x, b.z);
+                // our job for each concave slice is to find a reflex angle
+                // if we CAN'T find one then the slice is convex and we transfer it
+                // if we CAN, then we slice it
+                // in EITHER case we delete the slice
 
-                Vector2 bisector = (a2D+b2D).normalized;
-                // we have found our reflex angle and can now make a cut
+                // the loop that looks for a reflex angle
+                int reflexIndex = -1;
+                Vector2 bisector = Vector2.zero;
+                for (int i = 0; i < verts.Length; i++)
+                {
+                    Vector3 v1 = GetPreceedingVertex(verts3D, i);
+                    Vector3 v2 = verts[i];
+                    Vector3 v3 = GetSucceedingVertex(verts3D, i);
 
-                List<int>[] slices = CutPolygon(bisector, verts);
+                    Vector3 a = v1 - v2;
+                    Vector3 b = v3 - v2;
+
+                    float cAngle = GetClockwiseAngle(a, b, normal);
+
+                    if (cAngle >= 180)
+                    {
+                        reflexIndex = i;
+
+                        Vector2 a2D = new Vector2(a.x, a.z);
+                        Vector2 b2D = new Vector2(b.x, b.z);
+
+                        bisector = (a2D+b2D).normalized;
+                    }
+                }
+
+                if (reflexIndex == -1)
+                {
+                    // the slice is already convex so we just add it
+                    convexSlices.Add(concaveSlices[n]);
+                } else
+                {
+                    // here we have to cut the thing
+                    // each slice contains original vertex indices
+                    List<int>[] slices = CutPolygon(bisector, verts, concaveSlices[n]);
+                    
+                    concaveSlices.Add(slices[0].ToArray());
+                    concaveSlices.Add(slices[1].ToArray());
+                }
+                // again, we delete the concave slice in either case
+                concaveSlices.RemoveAt(n);
             }
         }
 
-
-        // now we have the slices array filled
+        // so now the concave slices list is at 0,
+        // and all we have are convex slices
         List<int[]> sliceTriangulations = new List<int[]>();
+        int triSum = 0;
         for (int i = 0; i < convexSlices.Count; i++)
         {
-            sliceTriangulations.Add(GenerateConvexTriangulation(convexSlices[i]));
+            int[] newTriangulation = GenerateConvexTriangulation(convexSlices[i].Length, concaveSlices[i]);
+
+            triSum += newTriangulation.Length;
+            sliceTriangulations.Add(newTriangulation);
+        }
+
+        // copying all of the different triangulations into one array
+        int[] result = new int[triSum];
+
+        int localIndex = 0;
+        int sliceIndex = 0;
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = sliceTriangulations[sliceIndex][localIndex];
+
+            if (localIndex == sliceTriangulations[sliceIndex].Length - 1)
+            {
+                localIndex = 0;
+                sliceIndex++;
+            } else {localIndex++;}
         }
 
         // temp
-        return new int[0];
+        return result;
+    }
+
+    // makes an array going [0, 1, 2, .... n] for n items
+    public static int[] GenerateAscending(int length)
+    {
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++)
+        {
+            result[i] = i;
+        }
+        return result;
     }
 
     // just easier to package up this 2-case logic
@@ -114,7 +193,9 @@ public class util_polygon
     // returns a 2 by n array that represents the two pieces of the cut polygon
 
     // very useful for turning a concave polygon into 2 convex gons
-    public static List<int>[] CutPolygon(Vector2 cutLine, Vector2[] verts)
+
+    // we don't need to return a util_polygoncomponent[] because we can just keep the original indices
+    public static List<int>[] CutPolygon(Vector2 cutLine, Vector2[] verts, int[] indexMap)
     {
         // using the idea that taking the negative rociprocal of the slope of a 2D line yields the perpindicular slope
         Vector2 perpLine = new Vector2(cutLine.y, -cutLine.x);
@@ -128,35 +209,45 @@ public class util_polygon
         {
             if (Vector2.Dot(perpLine, verts[i]) > 0)
             {
-                result[0].Add(i);
+                result[0].Add(indexMap[i]);
             } else if (Vector2.Dot(perpLine, verts[i]) < 0)
             {
-                result[1].Add(i);
+                result[1].Add(indexMap[i]);
             } else // lies right on the line, so 2 copies (bc we need the resulting copies to be complete polygons)
             {
-                result[0].Add(i);
-                result[1].Add(i);
+                result[0].Add(indexMap[i]);
+                result[1].Add(indexMap[i]);
             }
         }
 
         return result;
     }
+    // with no map
+    public static List<int>[] CutPolygon(Vector2 cutLine, Vector2[] verts)
+    {
+        return CutPolygon(cutLine, verts, GenerateAscending(verts.Length));
+    }
 
     // just a triangle fan, technically my own idea but many people have used this
     
     // not sure whether this is clockwise or not?
-    public static int[] GenerateConvexTriangulation(Vector2[] verts)
+    public static int[] GenerateConvexTriangulation(int length, int[] indexMap)
     {
-        int[] tris = new int[(verts.Length - 2) * 3];
+        int[] tris = new int[(length - 2) * 3];
 
         for (int i = 0, n = 1; i < tris.Length; i+= 3, n++)
         {
-            tris[i] = 0;
-            tris[i+1] = n;
-            tris[i+2] = n+1;
+            tris[i] = indexMap[0];
+            tris[i+1] = indexMap[n];
+            tris[i+2] = indexMap[n+1];
         }
 
         return tris;
+    }
+    // when there isn't a mapping
+    public static int[] GenerateConvexTriangulation(int length)
+    {
+        return GenerateConvexTriangulation(length, GenerateAscending(length));
     }
 
     // returns degrees

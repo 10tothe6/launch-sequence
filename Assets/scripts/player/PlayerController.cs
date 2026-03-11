4de4b7e6c1153cb16d2a713a88bfcc7c876a148c
Maximilian McDiarmid <maximilianmcdiarmid@icloud.com>
2026-03-11 13:18:18 -0400

    reorg

---
+++ Assets/scripts/player/PlayerController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+// one of the first projects in a long while not to use my classic FirstPersonController3D.cs class
+
+
+
+// this project is complicated enough that I'm writing another from scratch,
+// with the idea in mind that gravity can be any direction
+
+public class GeneralFP3DPlayerController : MonoBehaviour
+{
+    
+}

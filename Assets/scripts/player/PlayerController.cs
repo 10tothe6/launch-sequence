d11e1fcc8e31ff1893e532102ff5286f4359f722
Maximilian McDiarmid <maximilianmcdiarmid@icloud.com>
2026-04-16 11:41:14 -0400

    updated player controller variables, code still needs to be looked over

--- Assets/scripts/player/PlayerController.cs
+++ Assets/scripts/player/PlayerController.cs
@@ -16,23 +16,89 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region references
     private Rigidbody rb;
+    private RaycastHit hit;
+    public CapsuleCollider col;
+    // keep in mind that eventually I will have to create ghost copies of player entities (for moving vehicle physics)
+    // this means I'll need a controlling object of sorts, or similar system
+    #endregion
+
+    // since this is a multiplayer game, the player controller takes in this data class 
+    // instead of reading keypresses directly
     public player_keypresspacket lastPacket;
 
-    public float moveSpeed;
-    public float strafeSpeed;
-    public float turnSpeed;
+    # region flags
+    [Header("Flags")]
+    public bool allowJump = true;
+    public bool allowSprint = true;
+    // FLIGHT IS NOT FREECAM, THEY ARE DIFFERENT
+    // flight literally moves the robot model itself
+    public bool allowFlight = false; 
+    public bool allowCrouch = false;
+    # endregion
+
+    #region locks
+    [Header("Locks")]
+    [Header("Locks")]
+    public bool lockCameraHorizontal = false;
+    public bool lockCameraVertical = false;
+    public bool lockMovement = false;
+    public bool lockCursor = true;
+    #endregion
+
+    #region parameters
+    // used for raycast checks with the ground
+    public Transform t_foot;
+
+
+    // for now this applies to both the visual model and the collider, 
+    // but will eventually just be collider
+    public float crouchPercentHeight; 
+    private float colDefaultHeight;
+
+    public float moveSpeed; // moving forwards/backwards
+    public float strafeSpeed; // moving sideways
+    public float turnSpeed; // looking around
+
+    // the player's camera oscillates when walking, which for now is just done by changing the camera offset
+    // I likely won't change this, even after adding the player model - 
+    // though I may just parent the camera to a bone and do that instead
+    public float cameraBounceAmplitude;
+    public float cameraBounceFrequency;
+    private float defaultCameraHeight;
+    private float currentCameraHeight;
+
+
+
+    private bool isSprinting;
+    public float maxSprint; // this may need to change variably
+    [HideInInspector]
+    public float sprintValue; // the amount of sprint that the player has left
+    public LayerMask whatIsGround;
+    public LayerMask whatIsVehicle; // TODO: this, later (for mptest-3)
+    #endregion
+    
+    #region tracking variables
+     /* tracking variables */
+    private bool isFlying;
+    private bool isCrouching;
+
+    private bool activeJump;
+    private float sprintTimer;
+    /**/
+    #endregion
 
     void Awake()
     {
+        // setting references
         rb = GetComponent<Rigidbody>();
     }
 
     // just doing this through Update() and using Time.deltaTime instead of FixedUpdate()
     void Update()
     {
-        // temp temp temp
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked; // temp temp temp
 
         // here is where the familiar player-controller stuff starts to kick in
         Vector3 movementVector = Vector3.zero;
@@ -46,6 +112,8 @@ public class PlayerController : MonoBehaviour
         //rb.linearVelocity += movementVector * Time.deltaTime;
     }
 
+    // keypresses will not change unless updated, 
+    // meaning the game will think you are holding down keys if packets stop at the wrong time
     public void SetKeypresses(player_keypresspacket keys)
     {
         lastPacket = keys;

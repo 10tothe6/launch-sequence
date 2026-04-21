bfcfbfecf953222b4d5f6fc3cadc4cdc38c950f2
Maximilian McDiarmid <maximilianmcdiarmid@icloud.com>
2026-04-21 09:25:15 -0400

    added/removed the appropriate components from all prefabs

--- Assets/scripts/vfx/vfx_rocketflamecontroller.cs
+++ Assets/scripts/vfx/vfx_rocketflamecontroller.cs
@@ -2,5 +2,10 @@ using UnityEngine;
 
 public class vfx_rocketflamecontroller : MonoBehaviour
 {
-    
+    [Range(0,1)]
+    public float throttle;
+
+    public float ambientAirPressure; // in kPa
+
+    public bool isActive;
 }

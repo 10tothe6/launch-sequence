615b4fee4057b0448c6297373c872ec67d341111
Maximilian McDiarmid <maximilianmcdiarmid@icloud.com>
2026-04-22 17:10:49 -0400

    cable curve

--- Assets/scripts/_test/test_drawcatenary.cs
+++ Assets/scripts/_test/test_drawcatenary.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class test_drawcatenary : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Transform t1;
+    public Transform t2;
 
-    // Update is called once per frame
+    public Vector3[] points;
+    public float pointRadius;
+    public float sag;
+
+    public bool regen;
+    
     void Update()
     {
-        
+        if (regen)
+        {
+            points = util_geo.GetCatenaryPoints(t1.position, t2.position, sag).ToArray();
+           //regen = false;
+        }        
+    }
+
+    public void OnDrawGizmos()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            Gizmos.DrawSphere(points[i], pointRadius);
+        }
     }
 }
--- Assets/scripts/_util/util_geo.cs
+++ Assets/scripts/_util/util_geo.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 // i suspect a lot of geometry-related stuff in the future of this game,
@@ -7,6 +9,49 @@ using UnityEngine;
 
 public class util_geo
 {
+    /// <summary>
+    /// Generates a list of points representing a catenary curve between two 3D points.
+    /// </summary>
+    /// <param name="p1">Start point</param>
+    /// <param name="p2">End point</param>
+    /// <param name="slack">Extra length added to the minimum distance (must be > 0)</param>
+    /// <param name="segments">Number of points to generate</param>
+    public static List<Vector3> GetCatenaryPoints(Vector3 p1, Vector3 p2, float slack, int segments = 20)
+    {
+        List<Vector3> points = new List<Vector3>();
+
+        // 1. Find the midpoint between the two points
+        Vector3 midPoint = (p1 + p2) / 2f;
+
+        // 2. Create a "Control Point" by moving the midpoint straight down
+        // We subtract the sag from the Y axis to make it hang
+        Vector3 controlPoint = midPoint;
+        controlPoint.y -= slack;
+
+        // 3. Sample the Bezier Curve
+        for (int i = 0; i <= segments; i++)
+        {
+            float t = (float)i / segments;
+            points.Add(CalculateQuadraticBezier(p1, controlPoint, p2, t));
+        }
+
+        return points;
+    }
+
+    private static Vector3 CalculateQuadraticBezier(Vector3 start, Vector3 control, Vector3 end, float t)
+    {
+        // Standard Quadratic Bezier Formula: (1-t)^2 * P0 + 2(1-t)t * P1 + t^2 * P2
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+
+        Vector3 p = uu * start;       // (1-t)^2 * P0
+        p += 2 * u * t * control;     // 2(1-t)t * P1
+        p += tt * end;                // t^2 * P2
+
+        return p;
+    }
+
     // both directions must be normalized, obviously
     public static Vector3[] GenerateCirclePoints(Vector3 upDir, Vector3 rightDir, int pointCount, float radius)
     {

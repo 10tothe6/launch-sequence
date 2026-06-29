a4876832d3aec513a3ab98b01f07d5e128c812fc
Maximilian McDiarmid <maximilianmcdiarmid@icloud.com>
2026-06-29 09:11:29 -0400

    mockups for different spacecraft parts

---
+++ Assets/scripts/_crft/crft_battery.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+// any parts that have electricity
+
+public class crft_battery : MonoBehaviour
+{
+    public float maxElectricity;
+    public float ins_electricityAmt;
+
+    public void SetElectricityAmt()
+    {
+        
+    }
+
+    public void GetElectricityAmt()
+    {
+        
+    }
+
+    // can also be negative, if you want to remove electricity
+    public void AddElectricityAmt(float amtToAdd)
+    {
+        
+    }
+}
